0da48e6d9bdb0b46ace989137243a07b1d901d7a
Nico Bruenjes <nico.bruenjes@gmail.com>
2025-08-21 17:22:52 +0200

    feat: add prev next navigation

--- template-parts/content-prevnext.php
+++ template-parts/content-prevnext.php
@@ -5,22 +5,28 @@ $next_post = get_next_post();
 
 if ($prev_post || $next_post) : ?>
   <!-- content-prevnext.php -->
-  <section aria-labelledby="prev-next-posts-title">
+  <section aria-labelledby="prev-next-posts-title" class="article-extension">
     <h1 id="prev-next-posts-title"><?php esc_html_e('Next posts', 'reinfeld'); ?></h1>
     <div class="prev-next-posts">
       <?php if ($prev_post) : ?>
-        <p>
-          <a class="previous" href="<?php echo esc_url(get_permalink($prev_post)); ?>">
+        <div class="prev">
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 12">
+            <path fill-rule="evenodd" d="M4.06 4.94h10.425v2.12H4.061L7.5 10.5 6 12 .75 6.75 0 6l6-6 1.5 1.5z" />
+          </svg>
+          <a href="<?php echo esc_url(get_permalink($prev_post)); ?>">
             <?php echo esc_html(get_the_title($prev_post)); ?>
           </a>
-        </p>
+        </div>
       <?php endif; ?>
       <?php if ($next_post) : ?>
-        <p>
-          <a class="next" href="<?php echo esc_url(get_permalink($next_post)); ?>">
+        <div class="next">
+          <a href="<?php echo esc_url(get_permalink($next_post)); ?>">
             <?php echo esc_html(get_the_title($next_post)); ?>
           </a>
-        </p>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 12">
+            <path fill-rule="evenodd" d="M10.425 7.06H0V4.94h10.425L6.985 1.5l1.5-1.5 5.25 5.25.75.75-6 6-1.5-1.5z" />
+          </svg>
+        </div>
       <?php endif; ?>
     </div>
   </section>

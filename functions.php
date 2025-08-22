9997b5ce53041bc6fd865649c8213438efa4a17e
Nico Bruenjes <nico.bruenjes@gmail.com>
2025-08-22 13:40:49 +0200

    feat: add related posts section

--- functions.php
+++ functions.php
@@ -20,63 +20,88 @@ define('REINFELD_VERSION', '1.0.0');
 /**
  * Sets up theme defaults and registers support for various ClassicPress features.
  */
-function reinfeld_setup()
-{
-  // Add default posts and comments RSS feed links to head.
-  add_theme_support('automatic-feed-links');
+if (!function_exists('reinfeld_setup')):
+  /**
+   * Sets up theme defaults and registers support for various ClassicPress features.
+   */
+  function reinfeld_setup()
+  {
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support('automatic-feed-links');
 
-  // Let ClassicPress manage the document title.
-  add_theme_support('title-tag');
+    // Let ClassicPress manage the document title.
+    add_theme_support('title-tag');
 
-  // Enable support for Post Thumbnails on posts and pages.
-  add_theme_support('post-thumbnails');
+    // Enable support for Post Thumbnails on posts and pages.
+    add_theme_support('post-thumbnails');
 
-  // Add theme support for selective refresh for widgets.
-  add_theme_support('customize-selective-refresh-widgets');
+    // Post thumbnail size
+    set_post_thumbnail_size(1200, 9999);
 
-  // Add support for core custom logo.
-  add_theme_support(
-    'custom-logo',
-    array(
-      'height'      => 250,
-      'width'       => 250,
-      'flex-width'  => true,
-      'flex-height' => true,
-    )
-  );
-}
-add_action('after_setup_theme', 'reinfeld_setup');
+    // Custom image sizes
+    add_image_size('reinfeld_list-image', 640, 400);
 
+    // Title tag support
+    add_theme_support('title-tag');
+
+    // Add excerpts to pages
+    add_post_type_support('page', array('excerpt'));
+
+    // HTML5 semantic markup
+    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+
+    // Add theme support for selective refresh for widgets.
+    add_theme_support('customize-selective-refresh-widgets');
+
+    // Add support for core custom logo.
+    add_theme_support(
+      'custom-logo',
+      array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-width'  => true,
+        'flex-height' => true,
+      )
+    );
+  }
+  add_action('after_setup_theme', 'reinfeld_setup');
+endif;
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  */
-function reinfeld_content_width()
-{
-  $GLOBALS['content_width'] = apply_filters('reinfeld_content_width', 640);
-}
-add_action('after_setup_theme', 'reinfeld_content_width', 0);
+if (!function_exists('reinfeld_content_width')):
+  function reinfeld_content_width()
+  {
+    $GLOBALS['content_width'] = apply_filters('reinfeld_content_width', 640);
+  }
+  add_action('after_setup_theme', 'reinfeld_content_width', 0);
+endif;
 
 /**
  * Enqueue scripts and styles.
  */
-function reinfeld_scripts()
-{
-  // Enqueue fonts first
-  wp_enqueue_style('reinfeld-fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), REINFELD_VERSION);
+if (!function_exists('reinfeld_scripts')):
+  function reinfeld_scripts()
+  {
+    // Enqueue fonts first
+    wp_enqueue_style('reinfeld-fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), REINFELD_VERSION);
 
-  // Enqueue main stylesheet (depends on fonts)
-  wp_enqueue_style('reinfeld-style', get_stylesheet_uri(), array('reinfeld-fonts'), REINFELD_VERSION);
-}
-add_action('wp_enqueue_scripts', 'reinfeld_scripts');
+    // Enqueue main stylesheet (depends on fonts)
+    wp_enqueue_style('reinfeld-style', get_stylesheet_uri(), array('reinfeld-fonts'), REINFELD_VERSION);
+  }
+  add_action('wp_enqueue_scripts', 'reinfeld_scripts');
+endif;
 
 /**
  * Load theme textdomain for internationalization.
  */
-function reinfeld_textdomain()
-{
-  load_theme_textdomain('reinfeld', get_template_directory() . '/languages');
-}
-add_action('after_setup_theme', 'reinfeld_textdomain');
+if (!function_exists('reinfeld_textdomain')):
+  function reinfeld_textdomain()
+  {
+    load_theme_textdomain('reinfeld', get_template_directory() . '/languages');
+  }
+  add_action('after_setup_theme', 'reinfeld_textdomain');
+endif;
 
 /**
  * Customizer additions.
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -25,11 +25,6 @@
   <div class="entry-content">
     <?php
     the_content();
-
-    wp_link_pages(array(
-      'before' => '<div class="page-links">' . esc_html__('Pages:', 'reinfeld'),
-      'after'  => '</div>',
-    ));
     ?>
   </div>
 </article>
@@ -37,29 +32,8 @@
 <?php
 get_template_part('template-parts/content', 'prevnext');
 
-// Related Posts Sektion (vereinfacht)
-$related_posts = get_posts(array(
-  'numberposts' => 3,
-  'post__not_in' => array(get_the_ID()),
-  'category__in' => wp_get_post_categories(get_the_ID()),
-));
+get_template_part('template-parts/content', 'related');
 
-if ($related_posts) : ?>
-  <section class="related-posts">
-    <h2><?php esc_html_e('Related Posts', 'reinfeld'); ?></h2>
-    <ul>
-      <?php foreach ($related_posts as $related_post) : ?>
-        <li>
-          <a href="<?php echo esc_url(get_permalink($related_post)); ?>">
-            <?php echo esc_html(get_the_title($related_post)); ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </section>
-<?php endif; ?>
-
-<?php
 // Comments section
 if (comments_open() || get_comments_number()) :
   comments_template();

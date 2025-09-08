fd4136acad46a3c31b6581761b174dc0ac47dd55
Nico Bruenjes <nico.bruenjes@gmail.com>
2025-09-08 12:32:05 +0200

    feat: style comments area

--- functions.php
+++ functions.php
@@ -107,3 +107,83 @@ endif;
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
+
+
+if (! function_exists('reinfeld_comment')) :
+  function reinfeld_comment($comment, $args, $depth)
+  {
+
+    switch ($comment->comment_type):
+      case 'pingback':
+      case 'trackback':
+      case 'ping':
+      case 'like':
+      case 'repost':
+        global $post;
+?>
+
+        <li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+          <?php echo get_avatar($comment, 50, 'robohash'); ?>
+          <?php echo ucfirst($comment->comment_type) . ' ' . __('from', 'reinfeld') . ' ' . get_comment_author_link(); ?>
+
+        <?php
+
+        break;
+
+      default:
+        global $post;
+        ?>
+        <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+
+          <div id="comment-<?php comment_ID(); ?>">
+
+            <header class="comment-meta">
+
+              <span class="comment-author">
+                <?php echo get_avatar($comment, 50, 'robohash'); ?>
+                <cite>
+                  <?php echo get_comment_author_link(); ?>
+                </cite>
+
+                <?php
+                if ($comment->user_id === $post->post_author) {
+                  echo '<span class="comment-by-post-author"> (' . __('Author', 'reinfeld') . ')</span>';
+                }
+                ?>
+              </span>
+
+              <span class="comment-date">
+                <a class="comment-date-link" href="<?php echo esc_url(get_comment_link($comment->comment_ID)) ?>" title="<?php echo get_comment_date() . ' ' . __('at', 'reinfeld') . ' ' . get_comment_time(); ?>"><?php echo get_comment_date(get_option('date_format')); ?></a>
+              </span>
+              <pre><?php echo $depth; ?></pre>
+              <?php
+              comment_reply_link(array(
+                'after'      => '</span>',
+                'before'    => '<span class="comment-reply">',
+                'depth'      => $depth,
+                'max_depth'   => $args['max_depth'],
+                'reply_text'   => __('Reply', 'reinfeld'),
+              ));
+              ?>
+
+            </header>
+
+            <div class="comment-content entry-content">
+
+              <?php comment_text(); ?>
+
+            </div><!-- .comment-content -->
+
+            <div class="comment-actions">
+              <?php if ('0' == $comment->comment_approved) : ?>
+                <p class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'reinfeld'); ?></p>
+              <?php endif; ?>
+            </div><!-- .comment-actions -->
+
+          </div><!-- .comment -->
+
+  <?php
+        break;
+    endswitch;
+  }
+endif; // End if().

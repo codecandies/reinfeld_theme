e07ea3e048924881fc08d49674e0529a9d26956e
Nico Brünjes <nico.bruenjes@mailbox.org>
2026-05-11 13:55:22 +0200

    Refactor: Consolidate pagination into a template part

---
+++ template-parts/pagination.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Numbered posts pagination
+ *
+ * Used on index.php and archive.php.
+ * Relies on WordPress core the_posts_pagination().
+ *
+ * @package Reinfeld
+ */
+
+the_posts_pagination([
+    'mid_size'           => 2,
+    'prev_text'          => '&laquo; ' . esc_html__('Newer', 'reinfeld'),
+    'next_text'          => esc_html__('Older', 'reinfeld') . ' &raquo;',
+    'screen_reader_text' => esc_html__('Navigate to newer and older articles', 'reinfeld'),
+    'aria_label'         => esc_html__('Navigate to newer and older articles', 'reinfeld'),
+    'class'              => 'pagination',
+]);

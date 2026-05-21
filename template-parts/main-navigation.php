da2415505b918fb71dcdf11edb22b06320ac78fb
Nico Brünjes <nico.bruenjes@mailbox.org>
2026-05-21 12:21:57 +0200

    refactor: Remove hardcoded "Overview" link from main navigation

--- template-parts/main-navigation.php
+++ template-parts/main-navigation.php
@@ -7,7 +7,6 @@
 ?>
 <!-- main-navigation.php -->
 <nav class="mainnav" aria-label="<?php esc_attr_e("Main Navigation", "reinfeld"); ?>">
-  <a href="<?php echo esc_url(home_url("/")); ?>"><?php esc_html_e("Overview", "reinfeld"); ?></a>
 
   <?php // Configurable links via Appearance → Menus → Navigation (Footer)
 
